db93164b4f40aa30b3baa946f0243b177549bd7b
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-06-24 05:00:48 +0200

    See description
    Took a weighted approach compared to my version 1 where i did a bunch of ifs. This is still a basic version with limited amount of cats (3) will be extended soon and ui will be fixed looks ugly for now.

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from cat_data import cats
 
 app = Flask(__name__)
 
+def score_cat(cat, preferences):
+    score = 0
+    for trait, value in preferences.items():
+        if trait == "hair_length":
+            score += 1 if cat["traits"]["hair_length"] == value else 0
+        else:
+            score += cat["traits"].get(trait, 0) * value
+    return score
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('quiz.html')
 
-@app.route('/test')
-def e():
-    return "test"
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+    if request.method == 'POST':
+        preferences = {
+            "good_with_kids": 1.0 if request.form.get("kids") == "yes" else 0.0,
+            "good_with_other_animals": 1.0 if request.form.get("animals") == "yes" else 0.0,
+            "affectionate": 1.0 if request.form.get("cuddle") == "cuddly" else 0.0,
+            "independent": 1.0 if request.form.get("cuddle") == "independent" else 0.0,
+            "hair_length": request.form.get("hair_length")
+        }
+
+        scored = [(cat, score_cat(cat, preferences)) for cat in cats]
+        scored.sort(key=lambda x: x[1], reverse=True)
+        recommendations = [c[0] for c in scored[:3]]
+
+        return render_template('results.html', recommendations=recommendations)
+
+    return render_template('quiz.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ cat_data.py
@@ -0,0 +1,35 @@
+cats = [
+    {
+        "name": "Ragdoll",
+        "image": "ragdoll.jpg",
+        "traits": {
+            "good_with_kids": 1.0,
+            "good_with_other_animals": 0.9,
+            "affectionate": 1.0,
+            "independent": 0.1,
+            "hair_length": "long"
+        }
+    },
+    {
+        "name": "Sphynx",
+        "image": "sphynx.jpg",
+        "traits": {
+            "good_with_kids": 0.3,
+            "good_with_other_animals": 0.6,
+            "affectionate": 0.5,
+            "independent": 0.8,
+            "hair_length": "short"
+        }
+    },
+    {
+        "name": "British Shorthair",
+        "image": "british_shorthair.jpg",
+        "traits": {
+            "good_with_kids": 0.8,
+            "good_with_other_animals": 0.7,
+            "affectionate": 0.6,
+            "independent": 0.5,
+            "hair_length": "short"
+        }
+    }
+]
