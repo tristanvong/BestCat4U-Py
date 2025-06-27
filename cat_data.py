db4702771f21a53940ff1d50e5c0f30fae4ecdd9
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-06-27 12:30:00 +0200

    Added cats: bengal, oriental shorthair and siamese

--- cat_data.py
+++ cat_data.py
@@ -11,6 +11,17 @@ cats = [
         }
     },
     {
+        "name": "Bengal",
+        "image": "bengal.jpg",
+        "traits": {
+            "good_with_kids": 0.8,
+            "good_with_other_animals": 0.7,
+            "affectionate": 0.6,
+            "independent": 0.6,
+            "hair_length": "short"
+        }
+    },
+    {
         "name": "British Shorthair",
         "image": "british_shorthair.jpg",
         "traits": {
@@ -33,6 +44,17 @@ cats = [
         }
     },
     {
+        "name": "Oriental Shorthair",
+        "image": "oriental_shorthair.jpg",
+        "traits": {
+            "good_with_kids": 0.7,
+            "good_with_other_animals": 0.6,
+            "affectionate": 0.8,
+            "independent": 0.4,
+            "hair_length": "short"
+        }
+    },
+    {
         "name": "Ragdoll",
         "image": "ragdoll.jpg",
         "traits": {
@@ -41,6 +63,17 @@ cats = [
             "affectionate": 1.0,
             "independent": 0.1,
             "hair_length": "long"
+        }
+    },
+    {
+        "name": "Siamese",
+        "image": "siamese.jpg",
+        "traits": {
+            "good_with_kids": 0.7,
+            "good_with_other_animals": 0.6,
+            "affectionate": 0.9,
+            "independent": 0.3,
+            "hair_length": "short"
         }
     },
     {
